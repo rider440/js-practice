918e44117d1f76192ff884aad00d1ff187087ea4
Ankur Yadav <ayts7667@gmail.com>
2026-04-02 18:44:02 +0530

    learning1/4/2026

--- first1.js
+++ first1.js
@@ -1,17 +1,117 @@
-console.log("Hello + world!");
-var firstName = "John";
-var lastName = "Doe";
-console.log(firstName + " " + lastName);    
-console.log(firstName);
-// let var = "Test"; // This line is commented out to avoid syntax error
-let firstName1 = "Ankur Yadav";
-console.log(firstName1);
-// constant PI = 3.14; // This line is commented out to avoid syntax error
-const PI1 = 3.14;
-console.log(PI1);
-console.log(firstName1.length);
-console.log(firstName1[4]);
-console.log(firstName1.toUpperCase());
-console.log(firstName1.toLowerCase());
-console.log(firstName1.length - 1);
-console.log(firstName1[firstName1.length - 1]);
+// console.log("Hello + world!");
+// var firstName = "John";
+// var lastName = "Doe";
+// console.log(firstName + " " + lastName);    
+// console.log(firstName);
+// // let var = "Test"; // This line is commented out to avoid syntax error
+// let firstName1 = "Ankur Yadav";
+// console.log(firstName1);
+// // constant PI = 3.14; // This line is commented out to avoid syntax error
+// const PI1 = 3.14;
+// console.log(PI1);
+// console.log(firstName1.length);
+// console.log(firstName1[4]);
+// console.log(firstName1.toUpperCase());
+// console.log(firstName1.toLowerCase());
+// console.log(firstName1.length - 1);
+// console.log(firstName1[firstName1.length - 1]);
+
+// const odbj ={
+//     name: "Ankur Yadav",
+//     age: 24,
+//     city: "Delhi",
+//     isMarried: false,
+//     hobbies: ["Coding", "Traveling", "Cooking"],
+//     greet: function() {
+//         console.log(`Hello, my name is ${this.name}`);
+//     }
+// }
+// console.log(odbj);
+// console.log(odbj.name);
+// console.log(odbj.age);
+// console.log(odbj.city);
+// console.log(odbj.isMarried);
+// console.log(odbj.hobbies);
+// odbj.greet();
+
+// const fullname ={
+//     username : {
+//         fname : "Ankur",
+//         lname : "Yadav"
+//     }
+// }
+//     console.log(fullname.username.fname);
+
+// const obj1 = {
+//     name: "Ankur Yadav",
+//     age: 24,
+// }
+// const obj2 = {
+//     city: "Delhi",
+//     isMarried: false,
+// }
+// const mergedObj = {...obj1, ...obj2};
+// console.log(mergedObj);
+
+// const arrdeatils =[
+//     {
+//         id:1,
+//         name: "Ankur Yadav",
+//         age: 24
+//     },
+//     {
+//         id:2,
+//         name: "John Doe",
+//         age: 30
+//     },
+//     {
+//         id:3,
+//         name: "Jane Smith",
+//         age: 28
+//     },
+//     {
+//         id:4,
+//         name: "Alice Johnson",
+//         age: 35
+//     }
+
+// ]
+
+// function practices
+
+// console.log("Hello World!");
+
+// function saymyname(){
+//     console.log("Ankur Yadav");
+//     console.log("I am learning JavaScript");
+
+// }
+
+// saymyname();
+
+// function add(a,b){
+//    console.log(a+b);
+// }
+
+// add(5,"10"); // This will concatenate 5 and "10" resulting in "510"
+
+// add(5,null); 
+
+// function with object
+
+// const person = {
+//     name: "Ankur Yadav",
+//     age: 24
+// }
+
+// function greet(person){
+//     console.log(`Hello, my name is ${person.name} and I am ${person.age} years old.`);
+// }   
+// greet(person);
+
+// scope of variable
+// var x = 10; // Global scope
+// function test(){
+//     var y = 20; // Local scope
+//     console.log(x); // Accessing global variable
+//     console.log(y); // Accessing local variable  
---
+++ test.js
@@ -0,0 +1,28 @@
+const user = {
+    username :"Ankur",
+    price :999,
+
+    welcomemessage:function(){
+        console.log(`${this.username}, welcome to website`);
+    }
+
+}
+
+// user.welcomemessage()
+// user.username="yadav"
+// user.welcomemessage()
+
+const addtwo = (n1,n2)=> {
+    return n1+n2
+}
+// console.log(addtwo(25,65))
+
+// const addtwo1 =(n1,n2) => (n1+n2)
+
+// console.log(addtwo1(89,90))
+
+if (2==3){
+    console.log("true")
+    
+}
+else console.log("false")
